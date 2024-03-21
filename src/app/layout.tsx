bcfde9d61b4bce34b8032515d682fc57470a1e92
Ali Ibrahim <alibra71@gmail.com>
2024-03-21 10:33:34 +0300

    academy UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,14 @@
 "use client";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Layout from "../components/layout/layout";
 import * as React from "react";
-import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles";
+import { styled,Theme, CSSObject } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
 import CssBaseline from "@mui/material/CssBaseline";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import Navbar from "../components/appbar/navBar";
-import Image from "next/image";
 import Sidebar from "../components/sidebar/sidebar";
-import Paper from "@mui/material/Paper";
 const inter = Inter({ subsets: ["latin"] });
 
 // export const metadata: Metadata = {
